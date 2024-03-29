cc410ee29057da55c7c0ff8c319734d2beb42c18
nithyanand9969 <nithyanandnadar.m@gmail.com>
2024-03-29 16:26:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("nithanand");
+  const onchange = (event) => {
+    const newval = event.target.value;
+    setName(newval + "1");
+    console.log(setName);
+  };
+
+  console.log(name);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>{name}</label>
+
+      <input type="text" value={name} onChange={onchange}></input>
     </div>
   );
 }
